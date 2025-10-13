fab7894c2287b0ece7323d09ec9fa925452866bf
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2025-10-13 08:12:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from google.genai import types
 import PyPDF2
 from pptx import Presentation
 import docx
-from agentes import mostrar_aba_gerenciamento  # Mude "aba" para "agentes"
+from aba.agentes import mostrar_aba_gerenciamento  # Mude "aba" para "agentes"
 
 # Configuração inicial
 st.set_page_config(
